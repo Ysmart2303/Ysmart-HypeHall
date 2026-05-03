23ce1a92f3855c14489f18d4bbc6117a46bf0207
Ysmart2303 <viniciussaraiva3004@gmail.com>
2026-05-02 22:59:05 -0300

    new games e apps

--- js/index.js
+++ js/index.js
@@ -6,23 +6,50 @@ let senha = 2303;
 //logar
 
 function logar() {
-    let nomeU = document.getElementById("nomeUsu").value;
-    let senhaU = document.getElementById("senhaUsu").value;
 
-    if (!nomeU || !senhaU) {
-        nomeUsu.value = "";
-        senhaUsu.value = "";
-        alert("Preencha todos os campos");
-    } else if (nomeU == nome && senhaU == senha) {
-        nomeUsu.value = "";
-        senhaUsu.value = "";
-        alert(`Bem vindo senhor ${nomeU}!`);
-        mostrar();
+  let nomeU = document.getElementById("nomeUsu").value;
+  let senhaU = document.getElementById("senhaUsu").value;
+  
+  if (!nomeU || !senhaU) {
+    nomeUsu.value = "";
+    senhaUsu.value = "";
+    
+    nomeUsu.placeholder = "Preencha o campo 'nome'";
+    senhaUsu.placeholder = "Preencha o campo 'senha'";
+  } else if (nomeU == nome && senhaU == senha) {
+    nomeUsu.value = "";
+    senhaUsu.value = "";
+    alert(`Bem vindo senhor ${nomeU}!`);
+    mostrar();
+  } else {
+    nomeUsu.value = "";
+    senhaUsu.value = "";
+    alert("Nome ou senha incorretos");
+  }
+}
+
+//registrar
+function registrar() {
+    let nomeR = document.getElementById("REnomeUsu").value;
+    let senhaR = document.getElementById("REsenhaUsu").value;
+    let emailR = document.getElementById("REemailUsu").value;
+
+    if (!nomeR || !senhaR || !emailR) {
+      REnomeUsu.value = "";
+      REsenhaUsu.value = "";
+      REemailUsu.value = "";
+
+      REnomeUsu.placeholder = "Preencha o campo 'nome'";
+      REsenhaUsu.placeholder = "Preencha o campo 'senha'";
+      REemailUsu.placeholder = "Preencha o campo 'email'";
     } else {
-       nomeUsu.value = "";
-       senhaUsu.value = "";
-       alert("Nome ou senha incorretos");
+      REnomeUsu.value = "";
+      REsenhaUsu.value = "";
+      REemailUsu.value = "";
+      alert("A funcão de registro ainda não está disponível, tente novamente mais tarde.");
+      mostrarRegistro();
     }
+
 }
 
 // mostar
@@ -62,20 +89,30 @@ function mostrarRegistro() {
   }
 }
 
-//registrar
-function registrar() {
-    let nomeR = document.getElementById("REnomeUsu").value;
-    let senhaR = document.getElementById("REsenhaUsu").value;
-    let emailR = document.getElementById("REemailUsu").value;
+function moverEsquerda() {
+  const Jogos = document.getElementById("corpoJogos");
+  const Apps = document.getElementById("corpoApps");
 
-    if (!nomeR || !senhaR || !emailR) {
-        alert("Preencha todos os campos");
-    } else {
-      REnomeUsu.value = "";
-      REsenhaUsu.value = "";
-      REemailUsu.value = "";
-      alert("A funcão de registro ainda não está disponível, tente novamente mais tarde.");
-      mostrarRegistro();
-    }
+  if (Jogos.style.display === "flex") {
+    Jogos.style.display = "none";
+    Apps.style.display = "flex";
+  } else {
+    Jogos.style.display = "flex";
+    Apps.style.display = "none";
+  }
 
+}
+
+function moverDireita() {
+  const Jogos = document.getElementById("corpoJogos");
+  const Apps = document.getElementById("corpoApps");
+
+  if (Jogos.style.display === "none") {
+    Jogos.style.display = "flex";
+    Apps.style.display = "none";
+  } else {
+    Jogos.style.display = "none";
+    Apps.style.display = "flex";
+  }
+  
 }
